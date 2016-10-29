09d4c10128ef23e5800170c7efbb0e6d89dabed6
pkrebs01 <p.krebs@hotmail.com>
2016-10-29 16:02:18 -0700

    Add project files.

---
+++ TestDB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestDB.MvcApplication" Language="C#" %>
---
+++ TestDB/Models/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace TestDB.Models
+{
+    public class Item
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "Subject")]
+        public string Subject { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "semester")]
+        public string Semester { get; set; }
+       
+        [JsonProperty(PropertyName = "instructor")]
+        public string Instructor { get; set; }
+
+        [JsonProperty(PropertyName = "Completed")]
+        public bool Completed { get; internal set; }
+
+    }
+}
